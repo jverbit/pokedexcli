faaa4a481e5341db75acedc00634a005f820bb2b
JayVerb <lt.jverbit@gmail.com>
2026-09-02 12:43:18 -0400

    added missing commandExplore text from solution

--- command_explore.go
+++ command_explore.go
@@ -9,7 +9,8 @@ func commandExplore(cfg *config, args ...string) error {
 	if err != nil {
 		return err
 	}
-
+	fmt.Printf("Exploring %s...\n", locationMap.Name)
+	fmt.Println("Found Pokemon: ")
 	for _, encounter := range locationMap.Pokemon_encounters {
 		fmt.Println("-", encounter.Pokemon.Name)
 	}
